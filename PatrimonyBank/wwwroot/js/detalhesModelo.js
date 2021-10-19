cb0890da93304b3f6eee16ffe1468fc3cde3b0f1
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2021-10-18 21:41:19 -0300

    add melhorias na interface - metodo clean

--- PatrimonyBank/wwwroot/js/detalhesModelo.js
+++ PatrimonyBank/wwwroot/js/detalhesModelo.js
@@ -1,14 +1,23 @@
 const addId = document.querySelector('#addId');
 const cpfId = document.querySelector('#cpfId');
+const cnpjId = document.querySelector('#cnpjId');
 
-const InserirDetalhe = () => {
+const InserirCPF = () => {
     addId.style.display = "none";
     cpfId.style.display = "block";
     document.querySelector('#txtCpf').focus();
 }
 
+const InserirCNPJ = () => {
+    addId.style.display = "none";
+    cnpjId.style.display = "block";
+    document.querySelector('#txtCnpj').focus();
+}
+
 const OcultarModals = () => {
     addId.style.display = "flex";
     cpfId.style.display = "none";
+    cnpjId.style.display = "none";
     document.querySelector('#txtCpf').value = "";
+    document.querySelector('#txtCnpj').value = "";
 }
